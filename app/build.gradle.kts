7919081fee3938a9dd4d7f3739fa3e2d70de7017
eanassu <eanassu@gmail.com>
2025-04-14 19:53:51 -0300

    Aula 14/4 - Bancos de Dados (1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,12 +32,14 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.room:room-common:2.7.0")
+    implementation("androidx.room:room-runtime:2.7.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
+    annotationProcessor("androidx.room:room-compiler:2.7.0")
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
