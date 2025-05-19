8eb557ae81504be187720b23f4cd991aae6e7b6e
eanassu <eanassu@gmail.com>
2025-05-19 20:04:02 -0300

    Aula 19/05 - Desenho

--- app/src/main/java/br/fmu/projetoaula/DesenhoActivity.java
+++ app/src/main/java/br/fmu/projetoaula/DesenhoActivity.java
@@ -1,5 +1,9 @@
 package br.fmu.projetoaula;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,15 +14,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class DesenhoActivity extends AppCompatActivity {
 
+    private DesenhoView desenhoView;
+    private float acc;
+    private float currentAcc;
+    private float lastAcc;
+    private static final int ACC_LIMIT=5000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_desenho);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        desenhoView = findViewById(R.id.desenhoView);
+        SensorManager sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float x = event.values[0];
+                float y = event.values[1];
+                float z = event.values[2];
+                lastAcc = currentAcc;
+                currentAcc = x*x + y*y + z*z;
+                acc = currentAcc * (currentAcc-lastAcc);
+                if( acc > ACC_LIMIT ) {
+                    desenhoView.clear();
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {}
+        }, sensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
 }
